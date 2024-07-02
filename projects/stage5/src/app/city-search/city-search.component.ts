f8ea96ce0aeb3d4f34933a2e589325016e079f2d
Faizou <faizouaremou@gmail.com>
2024-07-02 07:55:51 +0200

    send request

--- projects/stage5/src/app/city-search/city-search.component.ts
+++ projects/stage5/src/app/city-search/city-search.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIcon } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
+import { WeatherService } from '../weather/weather.service'
 
 @Component({
   selector: 'app-city-search',
@@ -17,6 +18,20 @@ import { MatInputModule } from '@angular/material/input'
   templateUrl: './city-search.component.html',
   styleUrl: './city-search.component.css',
 })
-export class CitySearchComponent {
-  search = new FormControl()
+export class CitySearchComponent implements OnInit {
+  search = new FormControl<string>('')
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.search.valueChanges.subscribe((searchValue) => {
+      if (searchValue) {
+        const userInput = searchValue.split(',').map((s) => s.trim())
+        const result = this.weatherService.getCurrentWeather(
+          userInput[0],
+          userInput.length > 1 ? userInput[1] : undefined
+        )
+        result.subscribe()
+      }
+    })
+  }
 }
